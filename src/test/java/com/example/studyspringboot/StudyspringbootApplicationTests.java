a2ea93bc94fbbdbe32e1a11d1f72df4c00eed747
qkrthdud002 <qkrthdud002@gmail.com>
2022-07-26 22:47:13 +0900

    First Commit

---
+++ src/test/java/com/example/studyspringboot/StudyspringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.studyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("contentLoads...");
+	}
+
+}
