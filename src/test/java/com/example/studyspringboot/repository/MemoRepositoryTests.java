ae2d5f11f118efe93e215c7f81612dc77c8af6a3
qkrthdud002 <qkrthdud002@gmail.com>
2022-07-30 23:25:15 +0900

    0730

--- src/test/java/com/example/studyspringboot/repository/MemoRepositoryTests.java
+++ src/test/java/com/example/studyspringboot/repository/MemoRepositoryTests.java
@@ -1,7 +1,10 @@
 package com.example.studyspringboot.repository;
 
+import com.example.studyspringboot.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.stream.IntStream;
 
 public class MemoRepositoryTests {
 
@@ -11,6 +14,14 @@ public class MemoRepositoryTests {
     @Test
     public void testClass() {
         System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsertDummies() {
+        IntStream.rangeClosed(1, 100). forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample..." + i).build();
+            memoRepository.save(memo);
+        });
     }
 
 }
