9d66318a4fc1c6b0b8d7315436e339d1a850b627
qkrthdud002 <qkrthdud002@gmail.com>
2022-08-02 00:49:22 +0900

    0802

--- src/main/java/com/example/studyspringboot/repository/MemoRepository.java
+++ src/main/java/com/example/studyspringboot/repository/MemoRepository.java
@@ -3,8 +3,8 @@ package com.example.studyspringboot.repository;
 import com.example.studyspringboot.entity.Memo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
@@ -12,4 +12,6 @@ public interface MemoRepository extends JpaRepository<Memo, Long> {
     List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
 
     Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    void deleteMemoByMnoLessThan(Long num);
 }
--- src/test/java/com/example/studyspringboot/repository/MemoRepositoryTests.java
+++ src/test/java/com/example/studyspringboot/repository/MemoRepositoryTests.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -146,9 +147,16 @@ public class MemoRepositoryTests {
     public void testQueryMethodWithPageable() {
         Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
 
-        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, (java.awt.print.Pageable) pageable);
+        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
 
         result.get().forEach(memo -> System.out.println(memo));
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteQueryMethods() {
+        memoRepository.deleteMemoByMnoLessThan(10L);
     }
 
 }
