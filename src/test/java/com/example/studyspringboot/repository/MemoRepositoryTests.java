dca42c54dab337f337f191201588de32e3bfd9aa
qkrthdud002 <qkrthdud002@gmail.com>
2022-08-01 15:28:30 +0900

    0801

--- src/test/java/com/example/studyspringboot/repository/MemoRepositoryTests.java
+++ src/test/java/com/example/studyspringboot/repository/MemoRepositoryTests.java
@@ -3,9 +3,13 @@ package com.example.studyspringboot.repository;
 import com.example.studyspringboot.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
+@SpringBootTest
 public class MemoRepositoryTests {
 
     @Autowired
@@ -16,6 +20,7 @@ public class MemoRepositoryTests {
         System.out.println(memoRepository.getClass().getName());
     }
 
+    // 등록 작업 테스트
     @Test
     public void testInsertDummies() {
         IntStream.rangeClosed(1, 100). forEach(i -> {
@@ -25,5 +30,57 @@ public class MemoRepositoryTests {
                     .build();
             memoRepository.save(memo);
         });
+    }
+
+    // 조회 작업 테스트
+    @Test
+    public void testSelect() {
+
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("=================================="); // 나중에 출력됨.
+
+        if(result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    @Transactional
+    @Test
+    @Deprecated
+    public void testSelect2() {
+
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Memo memo = memoRepository.getOne(mno);
+
+        System.out.println("=================================="); // 먼저 출력됨.
+
+        System.out.println(memo);
+    }
+
+    // 수정 작업 테스트
+    @Test
+    public void testUpdate() {
+        Memo memo = Memo
+                .builder()
+                .mno(100L)
+                .memoText("Update Text")
+                .build();
+
+        System.out.println(memoRepository.save(memo));
+    }
+
+    // 삭제 작업 테스트
+    @Test
+    public void testDelete() {
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
     }
 }
