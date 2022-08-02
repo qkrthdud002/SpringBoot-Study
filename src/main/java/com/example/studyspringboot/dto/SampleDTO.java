987fcf580d5e1fe90f6aec4df41a7b73cdff7db7
qkrthdud002 <qkrthdud002@gmail.com>
2022-08-02 21:54:26 +0900

    0802

---
+++ src/main/java/com/example/studyspringboot/dto/SampleDTO.java
@@ -0,0 +1,19 @@
+package com.example.studyspringboot.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+}
