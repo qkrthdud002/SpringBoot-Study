a2271cb4721018b96aef0cb10459a4e34ee43388
qkrthdud002 <qkrthdud002@gmail.com>
2022-08-26 12:27:35 +0900

    0826

--- src/main/java/com/example/studyspringboot/controller/SampleController2.java
+++ src/main/java/com/example/studyspringboot/controller/SampleController2.java
@@ -60,7 +60,7 @@ public class SampleController2 {
         log.info("ex3");
     }
 
-    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate"})
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate", "/exSidebar"})
     public void exLayout1() {
 
         log.info("exLayout...........");
